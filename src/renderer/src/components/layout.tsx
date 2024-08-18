03eed69c3eeab7222f2ec03407f4e572f82c77b7
he.chunhui <499954367@qq.com>
2024-08-18 22:20:15 +0800

    update package

--- src/renderer/src/components/layout.tsx
+++ src/renderer/src/components/layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Fragment } from 'react/jsx-runtime'
 export default function Layout () {
   return (
-    <div>
+    <Fragment>
       <header></header>
-      <div>
+      <div className='wrapper'>
         <div className="lnb">lnb</div>
-        <div content='content'>
+        <div className='content'>
           <Outlet />
         </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './assets/main.css'
+import './scss/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as ReactDOM.Container).render(
   <React.StrictMode>
