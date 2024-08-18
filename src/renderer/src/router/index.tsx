b2d3cd62921c1c835727e47c28e60d36bbcf6387
he.chunhui <499954367@qq.com>
2024-08-18 21:44:50 +0800

    add file

--- src/renderer/src/router/index.tsx
+++ src/renderer/src/router/index.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from '../components/layout'
 
-const router = createBrowserRouter([{
+const router: any = createBrowserRouter([{
   path: '/',
   element: <Layout />,
   children: [
